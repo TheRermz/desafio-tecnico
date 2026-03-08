c6e6e4102c2b963b9ddafdeb4594c074f4d8b44b
Murilo <mufischer@hotmail.com>
2026-03-08 17:59:30 -0300

    feat(api): add entitys to orders module

--- api/src/order/order.module.ts
+++ api/src/order/order.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Items } from './entities/items.entity';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, Items, Product])],
   controllers: [OrderController],
   providers: [OrderService],
 })
-export class OrderModule {}
+export class OrderModule { }
