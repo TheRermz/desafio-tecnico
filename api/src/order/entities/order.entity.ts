d432c43583734d0dbf609288b67c8042b1ccd7aa
Murilo <mufischer@hotmail.com>
2026-03-08 17:29:04 -0300

    feat(api): create order entity and add uuid as dev dependency

--- api/src/order/entities/order.entity.ts
+++ api/src/order/entities/order.entity.ts
@@ -1,1 +1,31 @@
-export class Order {}
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  PrimaryColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { Item } from './item.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('order')
+export class Order {
+  @PrimaryColumn()
+  orderId: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  value: number;
+
+  @CreateDateColumn()
+  creationDate: Date;
+
+  @OneToMany(() => Item, (item) => item.order)
+  items: Item[];
+
+  @BeforeInsert()
+  generateOrderId() {
+    const uuid = uuidv4().replace(/-/g, '').substring(0, 8);
+    this.orderId = `v${uuid}-vdb-01`;
+  }
+}
