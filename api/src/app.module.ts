9294c1df5bb9acaa0b0f31d0bcb9548ffd0158a2
Murilo <mufischer@hotmail.com>
2026-03-08 16:53:14 -0300

    feat(api): add postgres support with TypeOrm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { OrderModule } from './order/order.module';
 
+const dbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'order',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
+
 @Module({
-  imports: [OrderModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(dbConfig), OrderModule],
 })
-export class AppModule {}
+export class AppModule { }
