c50b7ed02f707c357ccec32965382ec083ab831c
Murilo <mufischer@hotmail.com>
2026-03-08 21:02:50 -0300

    feat(api): create product repository

---
+++ api/src/order/repositories/product.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Product } from '../entities/product.entity';
+
+@Injectable()
+export class ProductRepository extends Repository<Product> {
+  constructor(private dataSource: DataSource) {
+    super(Product, dataSource.createEntityManager());
+  }
+
+  async findAllActive(): Promise<Product[]> {
+    return this.find({
+      where: { isActive: true },
+    });
+  }
+
+  async findById(productId: string): Promise<Product | null> {
+    return this.findOne({
+      where: { productId },
+    });
+  }
+
+  async deleteProduct(productId: string): Promise<void> {
+    await this.update(productId, { isActive: false });
+  }
+}
